6c3a55a402a1d8fdb276885f8eea34793e5ade14
Deekshitakatada <90176709+Deekshitakatada@users.noreply.github.com>
2021-09-21 21:51:18 +0530

    Exercise02_06

---
+++ src/Exercise02/Exercise02_06.java
@@ -0,0 +1,42 @@
+package Exercise02;
+
+import java.util.Scanner;
+
+public class Exercise02_06 {
+	public static void main(String[] args) {
+
+	     //printing a line to enter sum of digits 	
+		System.out.println("Enter an integer to discover the sum of its digits: ");
+
+		//making a scanner object
+	    Scanner input = new Scanner(System.in);
+
+	    //taking input integer from the user
+	    int Number = input.nextInt();           
+	    
+        //calling the function and displaying the ans
+        System.out.println(sumDigits(Number));
+        
+	}
+
+		//creating the function called sum digits
+	private static int sumDigits(int a) {
+
+	    //Initialising the sum variable to default value 0
+		int sum = 0;
+
+		//using a while loop to calculate the sum
+		while (a > 0) {
+			sum += (a % 10);
+	        a /= 10;    
+	        }
+		
+		//returning the calculated sum of digits
+		return sum;
+		
+	}
+
+
+}
+
+
