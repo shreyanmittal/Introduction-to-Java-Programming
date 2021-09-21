4720904588554e688e8532cfeb5ad263db1a35d1
20CS022_Ruhi <90175304+Ruhi-22@users.noreply.github.com>
2021-09-21 10:39:06 +0530

    Update Exercise02_05.java

--- src/Exercise02/Exercise02_05.java
+++ src/Exercise02/Exercise02_05.java
@@ -2,30 +2,26 @@ package Exercise02;
 
 import java.util.Scanner;
 /*
-BY - RUHI KANSAGARA
-ID - 20CS022
-Write a program that reads the subtotal and the gratuity rate, then computes the gratuity and total.
-For example, if the user enters 10 for subtotal and 15% for gratuity rate, the program displays $1.5 as gratuity and
-$11.5 as total.
- */
+* @author Ruhi Kansagara (20CS022)
+  Write a program that reads the subtotal and the gratuity rate, then computes the gratuity and total.
+  For example, if the user enters 10 for subtotal and 15% for gratuity rate, the program displays $1.5 as gratuity and
+  $11.5 as total.
+*/
+public class Exercise02_05 {
+    public static void main(String[] args) {
 
-    public class Exercise02_05 {
-        public static void main(String[] args) {
-
-            double subTotal;
-            double gratuityRate;
-            double total;
-            double gratuity;
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Enter subtotal and gratuity rate: ");
-            subTotal = sc.nextDouble();
-            gratuityRate = sc.nextDouble();
-            gratuity = (gratuityRate * subTotal)/100;
-            total = subTotal + gratuity;
-            System.out.println("The gratuity is: $"+gratuity);
-            System.out.println("The total amount is: $"+total);
-
-        }
-
+        double subTotal;
+        double gratuityRate;
+        double total;
+        double gratuity;
+        Scanner sc = new Scanner(System.in);                      //Input from user
+        System.out.println("Enter subtotal and gratuity rate: ");
+        subTotal = sc.nextDouble();                               //Scanning user input subTotal and gratuityRate
+        gratuityRate = sc.nextDouble();
+        gratuity = (gratuityRate * subTotal)/100;                 //Calculating gratuity
+        total = subTotal + gratuity;                              //Calculating total
+        System.out.println("The gratuity is: $"+gratuity);        //Printing the value of gratuity
+        System.out.println("The total amount is: $"+total);       //Printing the value of total
     }
+}
 
