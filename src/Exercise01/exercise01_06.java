ea4c7fe4418d369ba8eb334a889ddc7b7c61a990
KunjBapodariya <90176954+KunjBapodariya@users.noreply.github.com>
2021-09-21 20:38:06 +0530

    Update exercise01_06.java

--- src/Exercise01/exercise01_06.java
+++ src/Exercise01/exercise01_06.java
@@ -1,6 +1,5 @@
 /*
-(Summation of a series) Write a program that displays the result of
-				1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+(Summation of a series) Write a program that displays the result of 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
 */
 class Example {
     public static void main(String[] args) {
