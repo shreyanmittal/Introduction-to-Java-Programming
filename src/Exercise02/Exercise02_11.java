2afba4a0493d340b481b51510819bfde8ba8cb33
Ditsa1 <90301591+Ditsa1@users.noreply.github.com>
2021-09-21 13:37:45 +0530

    Update Exercise02_11.java

--- src/Exercise02/Exercise02_11.java
+++ src/Exercise02/Exercise02_11.java
@@ -1,22 +1,22 @@
 /*
-@author : Ditsa Mandani
-@email : <20cs031@charusat.ac.in>
-
-(Population projection) The U.S. Census Bureau projects population based on thefollowing assumptions:
-
-One birth every 7 seconds
-One death every 13 seconds
-One new immigrant every 45 seconds
-
-Write a program to display the population for each of the next five years.
-Assumethe current population is 312,032,486 and one year has 365 days. 
-Hint: In Java, if two  integers  perform  division,  the  result  is  an  integer.  
-The  fraction  part  is  trun-cated. 
-For example, 5/4 is 1 (not 1.25) and 10/4 is 2 (not 2.5).
-
-(Population  projection)  Rewrite  Exercise  1.11  to  prompt  the  user  to  enter  the number of years 
-and displays the population after the number of years.
-*/
+ * @author : Ditsa Mandani
+ * @email : <20cs031@charusat.ac.in>
+ *
+ * Exercise 1.11 (Population projection) The U.S. Census Bureau projects population based on thefollowing assumptions:
+ *
+ * One birth every 7 seconds
+ * One death every 13 seconds
+ * One new immigrant every 45 seconds
+ *
+ * Write a program to display the population for each of the next five years.
+ * Assumethe current population is 312,032,486 and one year has 365 days. 
+ * Hint: In Java, if two  integers  perform  division,  the  result  is  an  integer.  
+ * The  fraction  part  is  trun-cated. 
+ * For example, 5/4 is 1 (not 1.25) and 10/4 is 2 (not 2.5).
+ *
+ * Exercise 2.11 (Population  projection)  Rewrite  Exercise  1.11  to  prompt  the  user  to  enter  the number of years 
+ * and displays the population after the number of years.
+ */
 
 package Exercise02;
 
