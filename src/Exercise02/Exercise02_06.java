4f96f41021dd0dddd30f2b328ae6309290431aa8
Deekshitakatada <90176709+Deekshitakatada@users.noreply.github.com>
2021-09-21 19:15:45 +0530

    Update Exercise02_06.java

--- src/Exercise02/Exercise02_06.java
+++ src/Exercise02/Exercise02_06.java
@@ -4,23 +4,35 @@ import java.util.Scanner;
 
 public class Exercise02_06 {
 	public static void main(String[] args) {
-		System.out.println("Enter an integer to discover the sum of its digits: ");
+		
+	//printing a line to enter sum of digits 	
+	System.out.println("Enter an integer to discover the sum of its digits: ");
+		
+	//making a scanner object
         Scanner input = new Scanner(System.in);
-        int Number = input.nextInt();           //taking input from user
+		
+        //taking input integer from the user
+        int Number = input.nextInt();           
+		
+	//calling the function
         System.out.println(sumDigits(Number));
     }
 	
-	//method to calculate the sum
+	//creating the function called sumdigits
     private static int sumDigits(int a) {
-
+	    
+        //initialising the sum variable to default value 0
         int sum = 0;
+	    
+	//using a while loop to calculate the sum
         while (a > 0) {
         	
-        	sum += (a % 10);
-            a /= 10;
-            
+            sum += (a % 10);
+            a /= 10;    
         }
-        return sum;
+	    
+            //returning the calculated sum of digits
+	    return sum;
 	}
 
 }
