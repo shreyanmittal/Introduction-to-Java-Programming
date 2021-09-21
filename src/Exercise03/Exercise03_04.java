14a8eb7e9c76f63c826c31c9454c648a8b8701a2
kevincolaco13 <20cs013@charusat.edu.in>
2021-09-21 10:46:14 +0530

    Excercise03_04 by 20CS013

--- src/Exercise03/Exercise03_04.java
+++ src/Exercise03/Exercise03_04.java
@@ -2,6 +2,10 @@ package Exercise03;
 
 /*
  * @author Kevin Colaco (20CS013)
+ * 
+ * (Game: learn addition) Write a program that generates two integers under 100
+ *  and prompts the user to enter the sum of these two integers. The program then
+ *  reports true if the answer is correct, false otherwise.
  */
 
 import java.util.Scanner;
@@ -10,23 +14,24 @@ public class Exercise03_04 {
     public static void main(String[] args) {
         // Generating random numbers between 1 and 100
         // We have used int to convert the double value returned from random() to an integer
-        int number1 = (int)(Math.random() * 100);
-        int number2 = (int)(Math.random() * 100);
+        int randomNumber1 = (int)(Math.random() * 100);
+        int randomNumber2 = (int)(Math.random() * 100);
 
         // Printing the question for program
-        System.out.print("Give the answer for " + number1 + " + " + number2 + ": ");
+        System.out.print("Give the answer for " + randomNumber1 + " + " + randomNumber2 + ": ");
 
         // To get answer from user
         Scanner input = new Scanner(System.in);
         int answer = input.nextInt();
 
         // Condition to check if answer is right or wrong
-        if (number1 + number2 == answer) {
+        if (randomNumber1 + randomNumber2 == answer) {
             System.out.println("Your answer is correct, you have won the game");
         }
+
         else {
            System.out.println("Your answer is wrong");
-           System.out.println("The correct answer of " + number1 + " + " + number2 + " is " + (number1 + number2));
+           System.out.println("The correct answer of " + randomNumber1 + " + " + randomNumber2 + " is " + (randomNumber1 + randomNumber2));
            System.out.println("Better luck next time");
         }
     }
