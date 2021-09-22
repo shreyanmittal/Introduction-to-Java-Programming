202b8aee8959c498bd5fa6c5f6b2d4088a385004
Deekshitakatada <90176709+Deekshitakatada@users.noreply.github.com>
2021-09-22 09:20:19 +0530

    Update Exercise02_06.java

--- src/Exercise02/Exercise02_06.java
+++ src/Exercise02/Exercise02_06.java
@@ -2,6 +2,13 @@ package Exercise02;
 
 import java.util.Scanner;
 
+/*
+* @author Deekshitha katada (20CS024)
+* 2.6Write a program that reads an integer between 0 and
+* 1000 and adds all the digits in the integer.
+* For example, if an integer is 932, the sum of all its digits is 14.
+*/
+
 public class Exercise02_06 {
 	public static void main(String[] args) {
 
