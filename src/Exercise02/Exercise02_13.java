5ab1028762a4eeb00adb256053943e9d68ea5dc8
20CS033 <20cs033@charusat.edu.in>
2021-09-22 19:11:39 +0530

    Updated exrecise02_13
    This is the program of book chapter-2 exercise 2.13 by Mohit Marvania (20CS033)

--- src/Exercise02/Exercise02_13.java
+++ src/Exercise02/Exercise02_13.java
@@ -1,15 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+(Financial application: compound value) Suppose you save $100 each month into a savings account 
+with the annual interest rate 5%. Thus, the monthly inter- est rate is 0.05/12 = 0.00417. 
+After the first month, the value in the account becomes
+         100 * (1 + 0.00417) = 100.417
+After the second month, the value in the account becomes
+         (100 + 100.417) * (1 + 0.00417) = 201.252
+After the third month, the value in the account becomes
+         (100 + 201.252) * (1 + 0.00417) = 302.507
+and so on.
+Write a program that prompts the user to enter a monthly saving amount and displays the 
+account value after the sixth month. (In Exercise 4.30, you will use a loop to simplify 
+the code and display the account value for any month.)
+*/
 package Exercise02;
 
 import java.util.Scanner;
 
 /**
  *
- * @author mohit
+ * @author mohit  marvania
+ * 20CS033
  */
 public class Exercise02_13 {
     
