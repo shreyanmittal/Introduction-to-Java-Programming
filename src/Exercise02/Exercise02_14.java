f39c4688d638955f78ef355e8ba8d2f78759d837
Jainam2703 <20cs035@charusat.edu.in>
2021-09-24 13:18:29 +0530

    Update Exercise02_14.java

--- src/Exercise02/Exercise02_14.java
+++ src/Exercise02/Exercise02_14.java
@@ -18,9 +18,7 @@ public class Exercise02_14{
    public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
-
         System.out.println("Enter weight in pounds: ");
-        
         // Variable to store weight.
         double weight = in.nextDouble();
         
