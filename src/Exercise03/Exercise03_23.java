5f6ce01ea7abe3156f7517af13ebcfd0284183b3
Ditsa1 <20cs031@charusat.edu.in>
2021-09-26 21:19:07 +0530

    Update Exercise03_23.java

--- src/Exercise03/Exercise03_23.java
+++ src/Exercise03/Exercise03_23.java
@@ -7,7 +7,7 @@
  * and height 5. For example, (2,2) is inside the rectangle and (6,4) is outside the rectangle, as shown in Figure 3.9b. 
  * (Hint: A point is in the rectangle if its horizontal distance to (0,0) is less than or equal to 10 / 2
  * and its vertical distance to (0,0) is less than or equal to 5.0 / 2. Test your program to cover
- * all cases.) Here are two sample runs.
+ * all cases.)
  *
  */
 
