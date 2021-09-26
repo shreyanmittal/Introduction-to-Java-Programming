3882eb8eea58791f80a8fa5bc6a776ab904770a3
Shubhangkhemani05 <20cs027@charusat.edu.in>
2021-09-26 22:06:40 +0530

    Exercise04_15,Exercise04_18

---
+++ src/Exercise04/Exercise04_15.java
@@ -0,0 +1,18 @@
+package ch_04;
+
+import java.util.*;
+
+
+public class Exercise04_15 {
+    public static void main(String[] args) {
+        int start = '!';
+        int end = '~';
+
+        for (int i = start, j = 1; i <= end; i++, j++) {
+            if (j % 10 == 0) {
+                System.out.println();
+            }
+            System.out.print((char) i);
+        }
+    }
+}
