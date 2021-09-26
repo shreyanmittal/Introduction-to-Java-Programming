9a1dbc7d3d23048dc7d87c626a78c645fa8043e7
CS009 <20CS009@charusat.edu.in>
2021-09-26 15:11:23 +0530

    Updated Excercise01_09

--- src/Exercise01/Exercise01_09.java
+++ src/Exercise01/Exercise01_09.java
@@ -1,8 +1,12 @@
 package Exercise01;
-
-public class Exercise_01_09 {
-
-        public static void main(String[] args) {
+/* Write a program that displays the area and
+*perimeter of a rectangle with the width of 4.5 and height of 7.9 using the following formula:
+* area = width * height
+*/
+public class Exercise01_09
+{
+        public static void main(String[] args)
+        {
             //as stated in problem question
             double width = 4.5, height = 7.9;
 
@@ -10,8 +14,7 @@ public class Exercise_01_09 {
             double area = width*height;
             double perimeter = 2*(width+height);
 
+            //printing the answers
             System.out.println("The area of the rectangle is\n " + area + "and the perimeter is\n " + perimeter);
-
-    }
-
+        }
 }
