ba5139ccd120e2c59b929184cfbd150e707724e1
durgeshsamariya <samariya.durgesh@gmail.com>
2021-10-01 12:19:08 +0530

    Initial java file.

---
+++ src/Exercise07/Exercise07_01.java
@@ -0,0 +1,10 @@
+package Exercise07;
+
+public class Exercise07_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/Exercise08/Exercise08_01.java
@@ -0,0 +1,10 @@
+package Exercise08;
+
+public class Exercise08_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
