e695283a623ab3c53cb3576740d0beaf2b703db4
shreyanmittal <shreyanmittal75537@gmail.com>
2021-10-04 14:00:05 +0530

    Create Exercise04_12,Exercise04_13

---
+++ src/Exercise04/Exercise04_12.java
@@ -0,0 +1,16 @@
+public class Exercise_12 {
+
+    public static void main(String[] args) {
+
+        int n = 0;
+
+        while (Math.pow(n, 2) < 12000) {
+            n++;
+        }
+
+        System.out.println(n + " is the lowest number, such that n^2 is greater than 12,000");
+        System.out.println("Proof: " + (n - 1) + "^2 = " + Math.pow(n - 1, 2));
+        System.out.println("Proof: " + n + "^2 = " + Math.pow(n, 2));
+
+    }
+}
---
+++ src/Exercise04/Exercise04_13.java
@@ -0,0 +1,11 @@
+public static void main(String[] args) {
+    int n = 0;
+    int nCubed = (int) (Math.pow(n, 3));
+
+    while (nCubed < 12000) {
+        n++;
+    }
+    System.out.println("The highest integer below 12000 is " +n);
+ }
+
+}
